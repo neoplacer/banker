f71f5534fe675f8bdafe5dacec7612bf9c80c79c
Axel Hawker <axelhawker@gmail.com>
2016-03-03 04:11:05 +0000

    Added game route nested in users, added games controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resource :users do
+    resources :games
+  end
+  
 
   # You can have the root of your site routed with "root"
    root 'pages#home'
