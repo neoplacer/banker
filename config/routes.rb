117da6955f91ff1e2496a9b53e785f4919e9a12d
Axel Hawker <axelhawker@gmail.com>
2016-03-08 04:02:59 +0000

    Added nested route and controller for Game

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  
+  resources :users do
+    resource :game
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
