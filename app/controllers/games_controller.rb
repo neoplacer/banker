6a939362e0749e17668a98e5bd3657b5ddf9e2c8
Axel Hawker <axelhawker@gmail.com>
2016-03-08 04:19:48 +0000

    Added game create form and controller action (Profile Create Action)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,27 @@
-class ProfilesController < ApplicationController
+class GamesController < ApplicationController
 
+    def new
+        # form for user to create game details
+         @user = User.find( params[:user_id] )
+        @game = @user.build_game
+    end
+    
+    def create
+        @user = User.find( params[:user_id] )
+        @game = @user.build_game(game_params)
+        if @game.save
+          flash[:success] = "Game Created!"
+          redirect_to user_path( params[:user_id] )
+        else
+          render action: :new
+        end
+    end
+    
+    
+    private
+        def game_params
+          params.require(:game).permit(:gamename)
+        end
+    
+    
 end
