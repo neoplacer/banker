8cb9c9c3c4f554227649d103cbe6df2fc28303c6
Axel Hawker <axelhawker@gmail.com>
2016-03-11 04:49:02 +0000

    Cleaned up game creation. Now a user can edit and delete their games. They are also prevented from creating a new one if they already have one and other users should not be able to see their games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,19 @@
 class GamesController < ApplicationController
-
+    before_action :authenticate_user!
+    before_action :only_current_user
+    
+    def show
+        
+    end
+    
     def new
         # form for user to create game details
          @user = User.find( params[:user_id] )
+        if @user.game
+            redirect_to edit_user_game_path( params[:user_id])
+        else
         @game = Game.new
+        end
     end
     
     #Create a new game. If you already have a game it will overwrite it.
@@ -18,11 +28,38 @@ class GamesController < ApplicationController
         end
     end
     
+    def edit
+        @user = User.find( params[:user_id])
+        @game = @user.game
+    end
+    
+    def update
+        @user = User.find( params[:user_id])
+        @game = @user.game
+        if @game.update_attributes(game_params)
+            flash[:success] = "Game Updated!"
+            redirect_to user_path(params[:user_id])
+        else
+            render action :edit
+        end
+        
+    end
+    
+    def destroy
+        @user = User.find( params[:user_id])
+        @game = @user.game
+        @game.destroy
+        redirect_to user_path(params[:user_id])
+    end
     
     private
     # You need to sanitize all the params you are passing from your create form
         def game_params
           params.require(:game).permit(:gamename)
+        end
+        def only_current_user
+            @user = User.find( params[:user_id])
+            redirect_to(root_url) unless @user == current_user
         end
     
     
